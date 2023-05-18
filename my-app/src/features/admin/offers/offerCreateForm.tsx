3061a87d36682722d4250d4ecf66f76be36d7820
safetzabelii <93625327+safetzabelii@users.noreply.github.com>
2023-05-18 15:21:22 +0200

    commit

--- my-app/src/features/admin/offers/offerCreateForm.tsx
+++ my-app/src/features/admin/offers/offerCreateForm.tsx
@@ -1,4 +1,3 @@
-
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { ChangeEvent, useEffect, useState } from 'react';
@@ -10,41 +9,62 @@ import { observer } from 'mobx-react';
 import { useStore } from '../../../app/stores/store';
 import { OfferDto } from '../../../app/models/Menu/OfferDto';
 import { off } from 'process';
+import { Restaurant } from '../../../app/models/Menu/Restaurant';
+import { Menu } from '../../../app/models/Menu/Menu';
 
-export default observer(function OfferCreateForm(){
-  const {offerStore,modalStore,restaurantStore,menuItemStore, menuStore} = useStore();
+export default observer(function OfferCreateForm() {
+  const { offerStore, modalStore, restaurantStore, menuItemStore, menuStore } = useStore();
   const navigate = useNavigate();
   const [selectedMenu, setSelectedMenu] = useState('');
-  const [selectedRestaurant, setSelectedRestaurant] = useState('');
+  const [filteredMenus, setFilteredMenus] = useState<Menu[]>([]);
+  const [selectedRestaurant, setSelectedRestaurant] = useState<string>('');
   const [selectedItems, setSelectedItems] = useState<{ MenuItemId: number; Quantity: any; }[]>([]);
   const [items, setItems] = useState<{ MenuItemId: number; Quantity: any; }[]>([]);
-  const{createOffer}=offerStore;
+  const { createOffer } = offerStore;
+
 
   const [offer, setOffer] = useState<OfferDto>({
-    id:0,
+    id: 0,
     name: '',
     description: '',
     discountPercent: 0,
-    price:0,
+    price: 0,
     startDate: null,
     endDate: null,
-    restaurantId:'',
-    files:'',
+    restaurantId: '',
+    files: '',
   });
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
     menuStore.loadMenus().then(() => {
       menuItemStore.loadMenuItems();
-      setLoading(false); 
+      setLoading(false);
+      const filteredMenus = menuStore.MenuByName.filter((menu) => menu.restaurantId === selectedRestaurant);
+      setFilteredMenus(filteredMenus);
     });
-  }, [restaurantStore,menuItemStore]);
+  }, [restaurantStore, menuItemStore, selectedRestaurant]);
+
+  useEffect(() => {
+    if (selectedMenu) {
+      const menuItems = menuItemStore.getMenuItems.filter((menuItem) => menuItem.menuId === selectedMenu);
+      setItems(menuItems.map((menuItem) => ({ MenuItemId: menuItem.id, Quantity: 0 })));
+    } else {
+      setItems([]);
+    }
+  }, [selectedMenu]);
+  useEffect(() => {
+    restaurantStore.loadRestaurants().then(() => {
+      setLoading(false);
+    });
+  }, [restaurantStore]);
+  
+  
 
   const validationSchema = Yup.object({
     name: Yup.string().required('Name is required'),
   });
-  
+
   function handleFormSubmit(offer: OfferDto) {
     const formData = new FormData();
     formData.append('name', offer.name);
@@ -55,41 +75,35 @@ export default observer(function OfferCreateForm(){
     formData.append('price', offer.price!.toString());
     formData.append('startDate', offer.startDate!.toString());
     formData.append('endDate', offer.endDate!.toString());
-  
- 
-    formData.append('menuItemOffersJson', JSON.stringify(selectedItems));
-  
-    createOffer(formData)
-      .then(() => {
-        modalStore.closeModal();
-        navigate('dashboard/listOffers');
-      });
-  }
-  
 
- 
+    formData.append('menuItemOffersJson', JSON.stringify(selectedItems));
+
+    createOffer(formData).then(() => {
+      modalStore.closeModal();
+      navigate('dashboard/listOffers');
+    });
+  }
+
   const handleMenuItemChange = (e: ChangeEvent<HTMLInputElement>, MenuItemId: number) => {
     if (e.target.checked) {
-      setSelectedItems(prevItems => [...prevItems, { MenuItemId, Quantity: 0 }]);
+      setSelectedItems((prevItems) => [...prevItems, { MenuItemId, Quantity: 0 }]);
     } else {
-      setSelectedItems(prevItems => prevItems.filter(item => item.MenuItemId !== MenuItemId));
+      setSelectedItems((prevItems) => prevItems.filter((item) => item.MenuItemId !== MenuItemId));
     }
   };
-  
-  
+
   const handleQuantityChange = (e: ChangeEvent<HTMLInputElement>, MenuItemId: number) => {
     const newQuantity = parseInt(e.target.value);
-    setSelectedItems(prevItems =>
-      prevItems.map(item =>
-        item.MenuItemId === MenuItemId ? { ...item, Quantity: newQuantity } : item
-      )
+    setSelectedItems((prevItems) =>
+      prevItems.map((item) => (item.MenuItemId === MenuItemId ? { ...item, Quantity: newQuantity } : item))
     );
   };
-  
+
   const getMenuItemQuantity = (MenuItemId: number) => {
-    const selectedItem = selectedItems.find(item => item.MenuItemId === MenuItemId);
+    const selectedItem = selectedItems.find((item) => item.MenuItemId === MenuItemId);
     return selectedItem ? selectedItem.Quantity : 0;
   };
+
   return (
     <>
       {loading ? (
@@ -117,119 +131,117 @@ export default observer(function OfferCreateForm(){
                       id="name"
                       placeholder="Enter offer name"
                     />
+                    <ErrorMessage name="name" component="div" className="text-red-500 text-sm mt-1" />
+
+                    <label className="block text-black font-bold mb-2" htmlFor="description">
+                      Description:
+                    </label>
+                    <Field
+                      className="border border-gray-400 p-2 w-full rounded-md"
+                      type="text"
+                      name="description"
+                      id="description"
+                      placeholder="Enter offer description"
+                    />
                     <ErrorMessage
-                      name="name"
+                      name="description"
                       component="div"
                       className="text-red-500 text-sm mt-1"
                     />
-                    <label className="block text-black font-bold mb-2" htmlFor="description">
-                          Description:
-                        </label>
-                        <Field
-                          className="border border-gray-400 p-2 w-full rounded-md"
-                          type="text"
-                          name="description"
-                          id="description"
-                          placeholder="Enter offer description"
-                        />
-                        <ErrorMessage
-                          name="description"
-                          component="div"
-                          className="text-red-500 text-sm mt-1"
-                        />
-                      
-                    <label className="block text-black font-bold mb-2" htmlFor="discountPercent">
-                        Discount percent:
-                      </label>
-                      <Field
-                        className="border border-gray-400 p-2 w-full rounded-md"
-                        name="discountpercent"
-                        id="discountpercent"
-                        type="number"
-                        placeholder="Enter discount percent"
-                      />
-                      <ErrorMessage
-                        name="discountpercent"
-                        component="div"
-                        className="text-red-500 text-sm mt-1"
-                      />
-                  <label className="block text-black font-bold mb-2" htmlFor="price">
-                        Price:
-                      </label>
-                      <Field
-                        className="border border-gray-400 p-2 w-full rounded-md"
-                        type="number"
-                        name="price"
-                        id="price"
-                        placeholder="Enter price"
-                      />
-                      <ErrorMessage
-                        name="price"
-                        component="div"
-                        className="text-red-500 text-sm mt-1"
-                      />
 
-                      <label className="block text-black font-bold mb-2" htmlFor="startDate">
-                        Start Date:
-                      </label>
-                      <Field
-                        className="border border-gray-400 p-2 w-full rounded-md"
-                        name="startdate"
-                        id="startdate"
-                        type="date"
-                        placeholder="Enter start date"
-                        value={formik.values.startDate ? formik.values.startDate.toString().slice(0, 10) : ''}
-                      />
-                      <ErrorMessage
-                        name="startdate"
-                        component="div"
-                        className="text-red-500 text-sm mt-1"
-                      />
-      
+                    <label className="block text-black font-bold mb-2" htmlFor="discountPercent">
+                      Discount percent:
+                    </label>
+                    <Field
+                      className="border border-gray-400 p-2 w-full rounded-md"
+                      name="discountpercent"
+                      id="discountpercent"
+                      type="number"
+                      placeholder="Enter discount percent"
+                    />
+                    <ErrorMessage
+                      name="discountpercent"
+                      component="div"
+                      className="text-red-500 text-sm mt-1"
+                    />
+
+                    <label className="block text-black font-bold mb-2" htmlFor="price">
+                      Price:
+                    </label>
+                    <Field
+                      className="border border-gray-400 p-2 w-full rounded-md"
+                      type="number"
+                      name="price"
+                      id="price"
+                      placeholder="Enter price"
+                    />
+                    <ErrorMessage name="price" component="div" className="text-red-500 text-sm mt-1" />
+
+                    <label className="block text-black font-bold mb-2" htmlFor="startDate">
+                      Start Date:
+                    </label>
+                    <Field
+                      className="border border-gray-400 p-2 w-full rounded-md"
+                      name="startdate"
+                      id="startdate"
+                      type="date"
+                      placeholder="Enter start date"
+                      value={formik.values.startDate ? formik.values.startDate.toString().slice(0, 10) : ''}
+                    />
+                    <ErrorMessage
+                      name="startdate"
+                      component="div"
+                      className="text-red-500 text-sm mt-1"
+                    />
+
                     <ErrorMessage
                       name="selectedMenu"
                       component="div"
                       className="text-red-500 text-sm mt-1"
                     />
-                     <label className="block text-black font-bold mb-2" htmlFor="endDate">
-                       End Date:
-                  </label>
-                  <Field
-                    className="border border-gray-400 p-2 w-full rounded-md"
-                    id="enddate"
-                    type="date"
-                    name="endDate"
-                    placeholder="Enter end date"
-                    value={formik.values.endDate ? formik.values.endDate.toString().slice(0, 10) : ''}
-                  />
-                  <ErrorMessage
-                    name="enddate"
-                    component="div"
-                    className="text-red-500 text-sm mt-1"
-                  />
-                  
-                  <label className="block text-black font-bold mb-2" htmlFor="restaurantId">
-                      Restaurant:
-                  </label>
+
+                    <label className="block text-black font-bold mb-2" htmlFor="endDate">
+                      End Date:
+                    </label>
                     <Field
-                    as="select"
-                    id="restaurantId"
-                    name="restaurantId"
-                    className="border border-gray-400 p-2 w-full rounded-md"
-                  >
-                      <option value="">Select a restaurant</option>
-                    {restaurantStore.restaurantsByName.map((restaurant) => (
-                      <option key={restaurant.id!} value={restaurant.id!}>
-                        {restaurant.name}
-                      </option>
-                    ))}
-                  </Field>
-                    <ErrorMessage
-                    name="restaurantId"
-                    component="div"
-                    className="text-red-500 text-sm mt-1"
-                  />
-                  <label htmlFor="selectedMenu" className="block text-black font-bold mb-2">
+                      className="border border-gray-400 p-2 w-full rounded-md"
+                      id="enddate"
+                      type="date"
+                      name="endDate"
+                      placeholder="Enter end date"
+                      value={formik.values.endDate ? formik.values.endDate.toString().slice(0, 10) : ''}
+                    />
+                    <ErrorMessage name="enddate" component="div" className="text-red-500 text-sm mt-1" />
+
+                    <label className="block text-black font-bold mb-2" htmlFor="restaurantId">
+                      Restaurant:
+                    </label>
+                    <Field
+                          as="select"
+                          id="restaurantId"
+                          name="restaurantId"
+                          className="border border-gray-400 p-2 w-full rounded-md"
+                          value={selectedRestaurant}
+                          onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+                            setSelectedRestaurant(e.target.value);
+                            setSelectedMenu('');
+                            setSelectedItems([]);
+                          }}
+                        >
+                          <option value="">Select a restaurant</option>
+                          {restaurantStore.restaurantsByName.map((restaurant) => (
+                            <option key={restaurant.id!} value={restaurant.id!}>
+                              {restaurant.name}
+                            </option>
+                          ))}
+                        </Field>
+                        <ErrorMessage
+                          name="restaurantId"
+                          component="div"
+                          className="text-red-500 text-sm mt-1"
+                        />
+
+                    <label htmlFor="selectedMenu" className="block text-black font-bold mb-2">
                       Menu:
                     </label>
                     <Field
@@ -237,39 +249,38 @@ export default observer(function OfferCreateForm(){
                       id="selectedMenu"
                       name="selectedMenu"
                       className="border border-gray-400 p-2 w-full rounded-md"
-                      onChange={(e: ChangeEvent<HTMLSelectElement>) =>
-                        setSelectedMenu(e.target.value)
-                      }
+                      onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+                        setSelectedMenu(e.target.value);
+                        setSelectedItems([]);
+                      }}
+                      disabled={!selectedRestaurant}
                     >
-                      <option value="">Select a menu</option>
-                      {menuStore.MenuByName.map((menu) => (
-                        <option key={menu.id!} value={menu.id!}>
-                          {menu.name}
-                        </option>
+                       <option value="">Select a menu</option>
+                        {filteredMenus.map((menu) => (
+                          <option key={menu.id!} value={menu.id!}>
+                            {menu.name}
+                          </option>
                       ))}
                     </Field>
+
                     <label className="block text-black font-bold mb-2" htmlFor="files">
-                        Image:
-                      </label>
-                      <Field
-                        className="border border-gray-400 p-2 w-full rounded-md"
-                        type="file"
-                        name="files"
-                        id="files"
-                        placeholder="Upload restaurant image"
-                        accept="*"
-                      />
-                      <ErrorMessage
-                        name="files"
-                        component="div"
-                        className="text-red-500 text-sm mt-1"
-                      />
+                      Image:
+                    </label>
+                    <Field
+                      className="border border-gray-400 p-2 w-full rounded-md"
+                      type="file"
+                      name="files"
+                      id="files"
+                      placeholder="Upload restaurant image"
+                      accept="*"
+                    />
+                    <ErrorMessage name="files" component="div" className="text-red-500 text-sm mt-1" />
                   </div>
                 </div>
 
                 {/* Right Side */}
                 <div className="w-1/2 pl-4 border-l border-gray-400">
-                  {selectedMenu && (
+                {selectedMenu && (
                     <>
                       <label className="block text-black font-bold mb-2">Menu Items:</label>
                       {menuItemStore.getMenuItems
@@ -300,9 +311,10 @@ export default observer(function OfferCreateForm(){
                               className="border border-gray-400 p-2 w-full rounded-md"
                               type="number"
                               min="0"
-                              id={`menuItemQuantity-${menuItem.id}`}
-                              defaultValue={getMenuItemQuantity(menuItem.id)}
+                              id={`quantity-${menuItem.id}`}
+                              value={getMenuItemQuantity(menuItem.id)}
                               onChange={(e) => handleQuantityChange(e, menuItem.id)}
+                              disabled={!selectedItems.find((item) => item.MenuItemId === menuItem.id)}
                             />
                           </div>
                         ))}
@@ -311,14 +323,19 @@ export default observer(function OfferCreateForm(){
                 </div>
               </div>
 
-              <div className="flex justify-end space-x-4">
+              <div className="flex items-center justify-end mt-6">
                 <button
-                  className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition-colors duration-300 ease-in-out"
                   type="submit"
-                  disabled={!formik.isValid || formik.isSubmitting}
+                  className="bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 >
-                  {formik.isSubmitting ? 'Submitting...' : 'Submit'}
+                  Create Offer
                 </button>
+                <Link
+                  to="/dashboard/listOffers"
+                  className="ml-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                >
+                  Cancel
+                </Link>
               </div>
             </Form>
           )}
