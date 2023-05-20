cf14a7d9def607275ff2f45f20cea381484e5983
safetzabelii <93625327+safetzabelii@users.noreply.github.com>
2023-05-20 11:08:27 +0200

    admin dashboard

---
+++ my-app/src/features/admin/assets/statistics-chart.tsx
@@ -0,0 +1,80 @@
+import {
+  
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
+} from "@material-tailwind/react";
+import { color } from "@material-tailwind/react/types/components/alert";
+import PropTypes from "prop-types";
+import Chart from "react-apexcharts";
+
+interface props {
+  chart: any;
+  title: string;
+  description: string;
+  footer: any;
+}
+
+export function StatisticsChart({ chart, title, description, footer } : props) {
+  return (
+    <Card>
+      <CardHeader variant="gradient" color="blue">
+        <Chart {...chart}/>
+
+      </CardHeader>
+      <CardBody className="p-6">
+        <Typography variant="h6" color="blue-gray">
+          {title}
+        </Typography>
+        <Typography variant="small" className="font-normal text-blue-gray-600">
+          {description}
+        </Typography>
+      </CardBody>
+      {footer && (
+        <CardFooter className="border-t border-blue-gray-50 px-6 py-5">
+          {footer}
+        </CardFooter>
+      )}
+    </Card>
+  );
+}
+
+StatisticsChart.defaultProps = {
+  
+  footer: null,
+};
+
+StatisticsChart.propTypes = {
+  color: PropTypes.oneOf([
+    "white",
+    "blue-gray",
+    "gray",
+    "brown",
+    "deep-orange",
+    "orange",
+    "amber",
+    "yellow",
+    "lime",
+    "light-green",
+    "green",
+    "teal",
+    "cyan",
+    "light-blue",
+    "blue",
+    "indigo",
+    "deep-purple",
+    "purple",
+    "pink",
+    "red",
+  ]),
+  chart: PropTypes.object.isRequired,
+  title: PropTypes.node.isRequired,
+  description: PropTypes.node.isRequired,
+  footer: PropTypes.node,
+};
+
+StatisticsChart.displayName = "/src/widgets/charts/statistics-chart.jsx";
+
+export default StatisticsChart;
